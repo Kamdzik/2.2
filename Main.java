ea92a41850ae29f02bdc2616ab9d4deec3d287bf
Kamdzik <127319303+Kamdzik@users.noreply.github.com>
2023-03-22 13:48:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+   // System.out.println("Hello world!");
+    //+ * / - 
+    
+    double a=2;
+    double b=5;
+    double suma = a+b;
+    double odej = a-b;
+    double mnoz = a*b;
+    double dziel = a/b;
+    
+    System.out.println(" suma : "+ suma +" różnica: "+ odej + " iloczyn: " + mnoz + " dzielenie: " + dziel );
+    
+  }
+}
